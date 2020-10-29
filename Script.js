c4108839e6130c0ca584e3bb2fae328570005684
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-10-29 16:45:06 +0100

    tests image

--- Script.js
+++ Script.js
@@ -1,7 +1,15 @@
-var images = ["45", 4, 9, 16, 25];
+// var images = ["bird.jpg", "couple.jpg", "crocodile.jpg", "peaks.jpg"];
+var images = [
+    "https://cdn.pixabay.com/photo/2020/02/15/22/55/couple-4852225__340.jpg",
+    "https://cdn.pixabay.com/photo/2020/10/20/03/22/bird-5669389__340.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/16/10/04/crocodile-5176851__340.jpg",
+    "https://cdn.pixabay.com/photo/2020/10/11/09/04/peak-5645235__340.jpg"
+]
 
-numbers.forEach(afficherImages);
+images.forEach(afficherImages);
 
-function afficherImages(value, images) {
-  txt = txt + value + "<br>";
+function afficherImages(value, index, array) {
+    // let adresseImage = "images/" + value;
+    let baliseImage = '<img src=' + value + '">';
+    document.write(baliseImage);
 }
